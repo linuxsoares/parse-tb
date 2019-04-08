2c1b15416483c7c22a49e6e9017ab968c456ff4c
Plínio Balduino <pbalduino@noreply.github.com>
2019-04-08 15:05:46 +0200

    Melhorando o parser

--- src/parse_tb/core.clj
+++ src/parse_tb/core.clj
@@ -1,7 +1,8 @@
 (ns parse-tb.core
   (:require [parse-tb.parser :as parser]
             [clojure.pprint :as pp]
-            [instaparse.core :as insta])
+            [instaparse.core :as insta]
+            [clojure.string :as str])
   (:gen-class))
 
 (defn- ->intermediate-format [text]
@@ -10,18 +11,20 @@
 (defn -main [& args]
   (let [file (first args)
         parse  (insta/parser
-                    "document = chapter (br | empty-line+ | paragraph)+
-                     br = #'([\r]?[\n])+'
-                     paragraph = #'^' (italic | bold | text)+ (br | #'$')
-                     empty-line = #'^[ ]*$'
-                     chapter = '[chapter ' text ']'
-                     italic = &'::' (bold | text)+ '::'
-                     bold = '**' (italic | text)+ '**'
-                     text = !'::' (#'[A-ZÁÉÍÓÚÇÃÂÊÎÔÛÀa-z0-9áéíóúçãõâêîôûà.,!?\\-/: ]')+
+                    "document = chapter (<br> | empty-line+ | paragraph | quote)+
+                     br = <#'([\r]?[\n])+'>
+                     paragraph = <#'^'> (italic | bold | text)* (br | <#'$'>)
+                     quote = <'[quote '> (text | <br>)+ <']'>
+                     empty-line = <#'^[ ]*$'>
+                     chapter = <'[chapter '> text <']'>
+                     italic = <'℔'> (bold | text)+ <'℔'>
+                     bold = <'**'> (italic | text)+ <'**'>
+                     text = !'::' #'[A-ZÁÉÍÓÚÇÃÂÊÎÔÛÀa-z0-9áéíóúçãõâêîôûà.,;:!?\\-/\"\\'\\(\\) ]+'
                      ")]
     (-> file
         slurp
-        (parse :unhide :all)
+        (str/replace #"::" "℔")
+        (parse :trace true)
         pp/pprint)))
 
   ; (let [file (first args)]
