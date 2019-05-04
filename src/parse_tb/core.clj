c004d4e750b45e1690cdc92facedc86c04bb8b94
Plínio Balduino <pbalduino@noreply.github.com>
2019-05-04 20:57:35 +0200

    Refatoração do parser

--- src/parse_tb/core.clj
+++ src/parse_tb/core.clj
@@ -6,36 +6,35 @@
             [instaparse.core :as insta])
   (:gen-class))
 
-(defn -main [& args]
-  (let [filename (first args)
-        parse  (insta/parser
-                    "document = chapter (box | <br> | empty-line+ | paragraph | quote | img | index | label | section | code | title | todo | list)+
-                     box = <'[box '> (text | italic | monospaced)+ <']'> (<br> | empty-line+ | paragraph | index | code)+ <'[/box]'>
-                     code = <'[code' ' '?> ('javascript' | 'ruby' | 'clojure' | 'java')? text? (<' label='> text)? <']'> (ns-sym | code-text | br)* <'[/code]'>
-                     br = <#'([\r]?[\n])+'>
-                     paragraph = <#'^'> (italic | bold | monospaced | text | ref | ref-label)* (br | <#'$'>)
-                     quote = <'[quote '> (text | ref | italic | <br>)+ <']'>
-                     ref = <'[ref '> text <']'>
-                     ref-label = <'[ref-label '> text <']'>
-                     index = <'[index '> text <']'>
-                     label = <'[label '> text <']'>
-                     todo = <'[TODO '> (text | <br>)+ <']'>
-                     section = <'[section '> text <']'>
-                     title = <'[title '> text <']'>
-                     img = <'[img '> text <']'>
-                     list = <'[list]'>(text | monospaced | br)+<'[/list]'>
-                     empty-line = <#'^[ ]*$'>
-                     chapter = (<'[chapter' ' '?> text? <']'>)
-                     italic = <'℔'> (bold | text | monospaced)+ <'℔'>
-                     bold = <'ℨ'> (italic | text | monospaced)+ <'ℨ'>
-                     monospaced = <'℥'> text <'℥'>
-                     ns-sym = '℔' code-text
-                     text = #'[A-ZÁÉÍÓÚÇÃÂÊÎÔÛÀa-z0-9áéíóúçãõâêîôûà.,;:!?&\\-_/\"\\'`’\\(\\)%=#\\*\\+\\<>{}$²^\\\\~@▷◁ ]+'
-                     code-text = (!'[/code]' !'::' #'[A-ZÁÉÍÓÚÇÃÂÊÎÔÛÀa-z0-9áéíóúçãõâêîôûà.,;:!?&$\\-_/\"\\'\\(\\)%=#\\*\\+\\[\\]<>{}²^\\\\`|~@ ]{0,200}')+
-                     "  :output-format :enlive)
-        file   (java.io.File. filename)
-        out    (str/replace (.getName file) #"afc" "json")
-        result (-> file
+(def tubaina "document = chapter (box | <br> | empty-line+ | paragraph | quote | img | index | label | section | code | title | todo | list)+
+              box = <'[box '> (text | italic | monospaced)+ <']'> (<br> | empty-line+ | paragraph | index | code)+ <'[/box]'>
+              code = <'[code' ' '?> ('javascript' | 'ruby' | 'clojure' | 'java')? text? (<' label='> text)? <']'> (ns-sym | code-text | br)* <'[/code]'>
+              br = <#'([\r]?[\n])+'>
+              paragraph = <#'^'> (italic | bold | monospaced | text | ref | ref-label)* (br | <#'$'>)
+              quote = <'[quote '> (text | ref | italic | <br>)+ <']'>
+              ref = <'[ref '> text <']'>
+              ref-label = <'[ref-label '> text <']'>
+              index = <'[index '> text <']'>
+              label = <'[label '> text <']'>
+              todo = <'[TODO '> (text | <br>)+ <']'>
+              section = <'[section '> text <']'>
+              title = <'[title '> text <']'>
+              img = <'[img '> text <']'>
+              list = <'[list]'>(text | monospaced | br)+<'[/list]'>
+              empty-line = <#'^[ ]*$'>
+              chapter = (<'[chapter' ' '?> text? <']'>)
+              italic = <'℔'> (bold | text | monospaced)+ <'℔'>
+              bold = <'ℨ'> (italic | text | monospaced)+ <'ℨ'>
+              monospaced = <'℥'> text <'℥'>
+              ns-sym = '℔' code-text
+              text = #'[A-ZÁÉÍÓÚÇÃÂÊÎÔÛÀa-z0-9áéíóúçãõâêîôûà.,;:!?&\\-_/\"\\'`’\\(\\)%=#\\*\\+\\<>{}$²^\\\\~@▷◁ ]+'
+              code-text = (!'[/code]' !'::' #'[A-ZÁÉÍÓÚÇÃÂÊÎÔÛÀa-z0-9áéíóúçãõâêîôûà.,;:!?&$\\-_/\"\\'\\(\\)%=#\\*\\+\\[\\]<>{}²^\\\\`|~@ ]{0,200}')+")
+
+(defn- parse [filename grammar]
+  (println "Parsing" filename)
+
+  (let [parser (insta/parser grammar :output-format :enlive)
+        result (-> filename
                 slurp
                 (str/replace #"::" "℔")
                 (str/replace #"%%%%%", "℥%℥")
@@ -44,14 +43,14 @@
                 (str/replace #"℥\[\[", "℥▷▷")
                 (str/replace #"℥\[", "℥▷")
                 (str/replace #"]℥", "◁℥")
-                parse)]
-    (println "Parsing" (.getAbsolutePath file))
+                parser)]
+    result))
+
+(defn -main [& args]
+  (let [filename (first args)
+        result   (parse filename tubaina)
+        file   (java.io.File. filename)
+        out    (str/replace (.getName file) #"afc" "json")]
     (if (insta/failure? result)
       (pp/pprint result)
       (json/generate-stream result (io/writer (str "json/" out))))))
-
-  ; (let [file (first args)]
-  ;   (-> file
-  ;       slurp
-  ;       ->intermediate-format
-  ;       pp/pprint)))
