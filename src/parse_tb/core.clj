6ef42ba2aff4eff022eb525645c1b36c44876f32
Gilmar Soares <gilmar.soares@luizalabs.com>
2019-05-17 09:53:14 -0300

    Parse directory file

--- src/parse_tb/core.clj
+++ src/parse_tb/core.clj
@@ -46,11 +46,18 @@
                 parser)]
     result))
 
-(defn -main [& args]
-  (let [filename (first args)
-        result   (parse filename tubaina)
+(defn parse-file [filename]
+  (println "Parse " filename)
+  (let [result   (parse filename tubaina)
         file   (java.io.File. filename)
         out    (str/replace (.getName file) #"afc" "json")]
+    (println out)
     (if (insta/failure? result)
       (pp/pprint result)
       (json/generate-stream result (io/writer (str "json/" out))))))
+
+(defn -main [& args]
+  (let [folder (clojure.java.io/file (first args))
+        fs (file-seq folder)
+        files (map str (rest fs))]
+    (doall (map parse-file files))))
