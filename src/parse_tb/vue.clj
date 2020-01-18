8572f045eafc4367430241f45549962b9d09d062
Plínio Balduino <pbalduino@users.noreply.github.com>
2020-01-18 20:58:51 -0300

    Generate a complete page with Vue

--- src/parse_tb/vue.clj
+++ src/parse_tb/vue.clj
@@ -1,6 +1,10 @@
 (ns parse-tb.vue
-  (:require [clojure.string :refer [trim]])
+  (:require [clojure.string :as str])
   (:gen-class))
+
+(def chapnum (atom 0))
+(def secnum (atom 0))
+(def titnum (atom 0))
 
 (defn debug [value]
   (println ">>>>>>>>>>>>>>>>>>>>>>>>>>>" value))
@@ -8,18 +12,49 @@
 (defmulti parse (fn [node]
                   (:tag node)))
 
+(defmethod parse :bold [{:keys [content]}]
+  (str "<b>" (parse (first content)) "</b>"))
+
+(defmethod parse :box [{:keys [content]}]
+  (let [title (first content)
+        body  (rest content)]
+    (str "<box title=\"" (parse title) "\">\n"
+         (apply str (map parse body))
+         "</box>\n")))
+
 (defmethod parse :br [_])
 
 (defmethod parse :chapter [{:keys [content]}]
-  (str "<h1>" (parse (first content)) "</h1>\n"))
+  (reset! secnum 0)
+  (reset! titnum 0)
+  (str "<h1>" (str @chapnum ". " (parse (first content))) "</h1>\n"))
+
+(defmethod parse :code [{:keys [content]}]
+  (let [language (first content)
+        body     (rest content)]
+    (str "<snippet language=\"" language "\">"
+         (apply str (map parse body))
+         "</snippet>\n")))
+
+(defmethod parse :code-text [{:keys [content]}]
+  (str (str/replace (first content) #"<" "&lt;") "\n"))
 
 (defmethod parse :document [{:keys [content]}]
-  (str "<template>\n<page>\n"
+  (str "<template>\n<page>\n<wip />"
     (apply str (map parse content))
     "</page>\n</template>\n"))
 
+(defmethod parse :italic [{:keys [content]}]
+  (str "<i>" (parse (first content)) "</i>"))
+
 (defmethod parse :label [{:keys [content]}]
-  (str "<index entry=\"" (parse (first content)) "\" />\n"))
+  (str "<label entry=\"" (parse (first content)) "\" />\n"))
+
+(defmethod parse :monospaced [{:keys [content]}]
+  (str "<code>" (parse (first content)) "</code>"))
+
+(defmethod parse :ns-sym [{:keys [content]}]
+  (str "&colon;&colon;" (parse (second content))))
 
 (defmethod parse :paragraph [{:keys [content]}]
   (let [lines (apply str (map parse content))]
@@ -27,20 +62,32 @@
       (str "<p class=\"text-justify\">" lines "</p>\n"))))
 
 (defmethod parse :quote [{:keys [content]}]
-  
-  ; (str "<chapter-quote author="Alan Perlis" work=", primeiro ganhador do Turing Award">É melhor termos 100 funções operando em uma estrutura de dados do que 10 funções operando em dez estruturas</chapter-quote>)
-  (debug content)
-  )
+  (let [quote  (parse (first content))
+        who    (str/split (parse (second content)) #",")
+        author (str/trim (str/replace (first who) #"--" ""))
+        work   (some->> who
+                        second
+                        (str ","))]
+    (str "<chapter-quote author=\"" author "\" work=\"" work "\">" quote "</chapter-quote>\n")))
+
+(defmethod parse :section [{:keys [content]}]
+  (swap! secnum inc)
+  (reset! titnum 0)
+  (str "<h2>" (str @chapnum "." @secnum ". " (parse (first content)) "</h2>\n")))
 
 (defmethod parse :text [{:keys [content]}]
-  (-> content
-      first
-      trim))
+  (first content))
+
+(defmethod parse :title [{:keys [content]}]
+  (swap! titnum inc)
+  (str "<h3>" (str @chapnum "." @secnum "." @titnum ". " (parse (first content)) "</h3>\n")))
 
 (defmethod parse :default [{:keys [tag]}]
   (println "Tag " tag " not available yet"))
 
 (defn generate! [node file]
+  (reset! chapnum (Integer/parseInt (last (re-find #"^vue\/(\d+)" file))))
+  (println "File: " file)
   (spit "../descobrindoclojure/src/components/book/Data.vue"
     (-> node
         parse
